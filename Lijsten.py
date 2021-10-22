7534c596b2356fb88b218ccb74996fa4d581d7d3
MeesHellinga <92974276+MeesHellinga@users.noreply.github.com>
2021-10-22 10:07:07 +0200

    Delete Lijsten.py

--- Lijsten.py
+++
@@ -1,8 +0,0 @@
-mijn_dieren = []
-mijn_dieren.append("Trui", "Konijn")
-mijn_dieren.append("Timmy", "Konijn")
-mijn_dieren.append("Dori", "Konijn")
-mijn_dieren.append("Loet", "Hond")
-
-
-Print(mijn_dieren)
