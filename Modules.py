7c8f1d41aec98793e918d3d3ee21781fc94087d0
MeesHellinga <92974276+MeesHellinga@users.noreply.github.com>
2021-10-22 10:07:23 +0200

    Delete Modules.py

--- Modules.py
+++
@@ -1,4 +0,0 @@
-from time import sleep as pauze #importeer sleep en hernoem de functie tot pauze
-print("We wachten twee seconden")
-pauze(2) # de hernoemde sleep functie
-print("En nu zijn we terug")
